895dfa957e36befbf7d29a769da94db64380661b
Miguel Batista <bf.miguel@gmail.com>
2023-12-09 21:36:07 -0300

    mas plantillas

--- AppAlonger/urls.py
+++ AppAlonger/urls.py
@@ -1,10 +1,11 @@
 
 
 from django.urls import path
-from AppAlonger.views import crear_curso, show_html
+from AppAlonger.views import crear_curso, show_html, mostrar_cursos
 
 urlpatterns = [
     path('agregar_curso/', crear_curso),
     path('show/', show_html),
+    path('cursos/', mostrar_cursos),
 
 ]
--- AppAlonger/views.py
+++ AppAlonger/views.py
@@ -1,23 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from AppAlonger.models import Curso
 from django.http import HttpResponse
 # Create your views here.
 
+
+def mostrar_cursos(request):
+    cursos = Curso.objects.all()
+    contexto = {
+        "cursos" : cursos
+    }
+    return render(request, "AppAlonger/cursos.html", contexto)
+
 def crear_curso(request):
     curso = Curso(nombre= "python", camada="47785")
     curso.save()
-    contexto= {"curso": curso}
-    return render(request, 'index.html', contexto)
+    return redirect("/app/cursos/")
 
 def show_html(request):
     curso = Curso.objects.all()
-    contexto = {"curso": curso}
+    contexto = {"curso": curso, "nombre": "Miguel"}
     return render(request, 'index.html', contexto)
 
 
 
-"""
-tambien puedo hacer 
-return HttpResponse(curso.nombre)
-
-"""
